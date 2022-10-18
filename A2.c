e2dd41cdf861d56185bca501f0aa00147c7cb297
Boomni Jonathan <rejoiceoye1@gmail.com>
2022-10-18 11:29:16 +0100

    o

---
+++ A2.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+int getnumber();
+
+/**
+ * main - generates fibonacci numbers lessthan or equals to 'n'
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int input = 0;
+	int n1 = 0, n2 = 1, n3;
+	int i = 0;
+
+	input = getnumber();
+	for (i = 0; i <= input; i++)
+	{
+		n3 = n1 + n2;
+		n1 = n2;
+		n2 = n3;
+		if (n3 <= input)
+			printf("%d, ", n3);
+	}
+	putchar('\n');
+	return 0;
+}
+
+int getnumber()
+{    
+	int number;
+
+	printf("Enter a number: ");
+	scanf("%d", &number);
+	return (number);
+}
+
+
